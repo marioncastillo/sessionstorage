a10c1bf228d078c131265dd2bfbd9678c189d2a2
MarionFoto <marionacp@gmail.com>
2017-06-18 22:54:55 -0400

    oook

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 function saveData(){
-	var name=document.getElemenById("name").value;
-	var clave=document.getElemenById("clave").value;
+	var name = document.getElementById("name").value;
+	var clave = document.getElementById("clave").value;
 	sessionStorage.setItem(name,clave);
 }
 
-function recoverData{
-	for(var i=0;i<sessionStorage.length;i++){
-		var name=sessionStorage.key(i);
-		var clave=sessionStorage.getItem(name);
-		document.getElemenById("data").innerHTML = '<div>' + 'Nombre : ' + name + '<br/> Clave : ' +clave+ '<div>';
+function recoverData(){
+	for (var i =0;i<sessionStorage.length;i++) {
+		var name = sessionStorage.key(i);
+		var clave = sessionStorage.getItem(name);
+		document.getElementById("data").innerHTML = "<div>" + "Nombre : " + name + "</br> Clave : " + clave + "</div>"; 
 	}
 }
 
 function cleanData(){
-	document.getElemenById("data").innerHTML='Limpiada Vista.Los datos permanecen.';
+	document.getElementById("data").innerHTML = "Limpiada vista. Los datos permanecen."
 }
 
 function borrarTodo(){
